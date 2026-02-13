21920be86c5e8bbc41e83548a279e51c4ceb7cbd
zane <czl970721@gmail.com>
2026-02-13 15:32:37 -0500

    refactor(core): update exports with Session and SharedContext

--- src/picklebot/core/__init__.py
+++ src/picklebot/core/__init__.py
@@ -4,13 +4,15 @@ from .agent import Agent
 from .context import SharedContext
 from .history import HistoryMessage, HistorySession, HistoryStore
 from .session import AgentSession
+from .session_new import Session
 
 
 __all__ = [
     "Agent",
-    "AgentSession",
+    "AgentSession",  # Temporary, will be removed
+    "Session",
+    "HistoryStore",
     "HistoryMessage",
     "HistorySession",
-    "HistoryStore",
     "SharedContext",
 ]
