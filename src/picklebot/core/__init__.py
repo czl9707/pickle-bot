a71661bb74347594d699a0bf3023f425267fbb2b
zane <czl970721@gmail.com>
2026-02-14 21:30:58 -0500

    feat(core): export AgentDef and AgentLoader

--- src/picklebot/core/__init__.py
+++ src/picklebot/core/__init__.py
@@ -1,15 +1,21 @@
 """Core agent functionality."""
 
 from .agent import Agent, AgentSession
+from .agent_def import AgentBehaviorConfig, AgentDef
+from .agent_loader import AgentLoader, AgentNotFoundError, InvalidAgentError
 from .context import SharedContext
 from .history import HistoryMessage, HistorySession, HistoryStore
-
 
 __all__ = [
     "Agent",
     "AgentSession",
+    "AgentDef",
+    "AgentBehaviorConfig",
+    "AgentLoader",
+    "AgentNotFoundError",
+    "InvalidAgentError",
+    "SharedContext",
     "HistoryStore",
     "HistoryMessage",
     "HistorySession",
-    "SharedContext",
 ]
