54785cf24b14aa6e56fd668fd8f30614c31f8a26
zane <czl970721@gmail.com>
2026-02-15 23:29:02 -0500

    feat: export def_loader utilities from utils package

--- src/picklebot/utils/__init__.py
+++ src/picklebot/utils/__init__.py
@@ -1,1 +1,17 @@
-"""Utility functions for pickle-bot."""
+"""Utilities package."""
+
+from picklebot.utils.def_loader import (
+    DefNotFoundError,
+    InvalidDefError,
+    discover_definitions,
+    parse_frontmatter,
+)
+from picklebot.utils.logging import setup_logging
+
+__all__ = [
+    "DefNotFoundError",
+    "InvalidDefError",
+    "discover_definitions",
+    "parse_frontmatter",
+    "setup_logging",
+]
