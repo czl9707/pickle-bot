dceb2325b6b57f65a0e3b16f30477ae6a6634af4
zane <czl970721@gmail.com>
2026-02-18 21:23:00 -0500

    test: remove trivial test_context.py

--- tests/core/test_context.py
+++
@@ -1,8 +0,0 @@
-"""Tests for SharedContext."""
-
-
-def test_shared_context_holds_config_and_history_store(test_context):
-    """SharedContext should hold config and history_store."""
-    assert test_context.config is not None
-    assert test_context.history_store is not None
-    assert test_context.agent_loader is not None
